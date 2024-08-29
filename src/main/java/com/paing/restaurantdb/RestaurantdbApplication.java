5efaab1861c558b8a13da090441fd6a2faa66c09
Your Name~ <hmphtetminpaing@gmail.com>
2024-08-29 20:25:40 +0630

    Initial commit

---
+++ src/main/java/com/paing/restaurantdb/RestaurantdbApplication.java
@@ -0,0 +1,13 @@
+package com.paing.restaurantdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantdbApplication.class, args);
+	}
+
+}
